8576d79f80634a68b4b5042064eb27de2b0d2eed
maeldroem <maeldroem@proton.me>
2026-05-17 00:24:20 +0200

    Initialized C# API project

---
+++ apis/csharp-impl/DemoBlackjack/Api/Program.cs
@@ -0,0 +1,27 @@
+using OpenApiUi;
+
+namespace Api;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseOpenApiUi(config => config.OpenApiSpecPath = "/openapi/v1.json");
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.Run();
+    }
+}
